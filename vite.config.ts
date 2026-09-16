0f93050a9a33c7891384207c9f597b58e8d6e57e
tkoints <akani.peak@gmail.com>
2026-09-16 22:47:17 +0300

    increase PWA cache

--- vite.config.ts
+++ vite.config.ts
@@ -38,6 +38,7 @@ export default defineConfig({
         globPatterns: ["**/*.{js,css,html,svg,png,ico,woff2}"],
         cleanupOutdatedCaches: true,
         clientsClaim: true,
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
       },
       devOptions: { enabled: false },
     }),
